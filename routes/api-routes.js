c7e4d0cc49273be25547c3f560a8b062e43178ce
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-07-15 20:49:26 -0700

    sooo stupid

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,7 +8,7 @@ const { Op } = require("sequelize");
 // =============================================================
 const character = require("../models/character.js");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
