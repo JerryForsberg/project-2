f8b215de2ed9441aaeba586dea9452e1249c6f7e
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-07-15 21:01:57 -0700

    added to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO characters (`name`, race, class, `level`, hp, strength,dexterity, weapon) VALUES ('Dale', 'Dragonborn','Fighter', 12, 8,10,8, "Sickle");
+INSERT INTO characters (`name`, race, class, `level`, hp, strength,dexterity, weapon) VALUES ('Jerry', 'Elf', 'Wizard', 12,8, 10, 8,'Longbow');
+INSERT INTO characters  (`name`, race, class, `level`, hp, strength,dexterity, weapon) VALUES ('Kaelyn', 'Half-Elf', 'Druid', 12,8,10,8,'Dagger');
+INSERT INTO characters  (`name`, race, class, `level`, hp, strength,dexterity, weapon) VALUES ('Isaac', 'Lizardfolk', 'Ranger', 12,8,10,8,'Sling');
