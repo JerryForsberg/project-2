6a3b9ada200d129cc61184b6feedd9626fdc2c51
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-07-16 17:55:08 -0700

    routes now use handlebars not html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,27 +24,18 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("index");
   });
 
   app.get("/charSel", (req, res) => {
-    if (req.user) {
-      res.redirect("/charSel");
-    }
     res.render("charSel");
   });
 
   app.get("/create", (req, res) => {
-    if (req.user) {
-      res.redirect("/create");
-    }
     res.render("create");
   });
 
   app.get("/choose", (req, res) => {
-    if (req.user) {
-      res.redirect("/choose");
-    }
     res.render("choose");
   });
 
