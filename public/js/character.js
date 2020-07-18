f022cadda96c085f6b08d14fe2bed7d71a114853
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-07-18 10:42:04 -0700

    pair coding

--- public/js/character.js
+++ public/js/character.js
@@ -1,33 +1,33 @@
 // const character = require("../../models/character");
 
 $(document).ready(() => {
-  // create character store to db
-  // $(function () {
-  //     $(".createChar").on("click", function(event) {
-  //          event.preventDefault();
+//   create character store to db
+  $(function () {
+      $(".createChar").on("click", function(event) {
+           event.preventDefault();
 
-  //         const newChar = {
-  //            name: $(this).data("name"),
-  //            race: $(this).data("race"),
-  //            class: $(this).data("class"),
-  //            level: $(this).data("level"),
-  //            hp: $(this).data("hp"),
-  //            strength: $(this).data("strength"),
-  //            dexterity: $(this).data("dexterity"),
-  //            weapon: $(this).data("weapon"),
-  //            selected: $(this).data(true)
-  //         };
+          const newChar = {
+             name: $(this).data("name"),
+             race: $(this).data("race"),
+             class: $(this).data("class"),
+             level: $(this).data("level"),
+             hp: $(this).data("hp"),
+             strength: $(this).data("strength"),
+             dexterity: $(this).data("dexterity"),
+             weapon: $(this).data("weapon"),
+             selected: $(this).data(true)
+          };
 
-  //         $.ajax("/api/create/", {
-  //             type: "POST",
-  //             data: newChar
-  //         }).then(
-  //             function() {
-  //                 window.location.href = "/charSel";
-  //             }
-  //         );
-  //     });
-  // });
+          $.ajax("/api/create/", {
+              type: "POST",
+              data: newChar
+          }).then(
+              function() {
+                  window.location.href = "/charSel";
+              }
+          );
+      });
+  });
 
   // selectChar is the user clicking to select the character
 
